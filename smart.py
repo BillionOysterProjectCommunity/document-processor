b6c40d422a3ab5c9c0a6f122e3440f5159595cf3
GlennTatum <glennetatum@gmail.com>
2023-11-11 12:19:10 -0500

    Delete smartsheet dependency as it can not be used

--- smart.py
+++
@@ -1,9 +0,0 @@
-import smartsheet
-from config import Config
-
-# TODO API Access requires "upgraded" plan for smartsheet
-
-class Smart:
-    def __init__(self, config):
-        self.config = config
-        self.client = smartsheet.Smartsheet(access_token=self.config.read('smartsheet-access-token'))
