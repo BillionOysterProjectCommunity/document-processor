72403180dc4a25d0b1a43fc6e406e09661e62adf
GlennTatum <glennetatum@gmail.com>
2024-01-22 21:48:43 -0500

    use uswds styles and force "date" type on monitoring date field

--- theia/web/forms.py
+++ theia/web/forms.py
@@ -9,13 +9,15 @@ from wtforms.fields import (
     IntegerField
 )
 
+from wtforms.widgets.core import DateInput
+
 from wtforms.validators import DataRequired
 
 # NOTE: field data is accessed through form.field.data
 
 class MetadataForm(FlaskForm):
     tag_type = SelectField('Tag Type', choices=['Rectangle', 'Circle'])
-    monitoring_date = DateField('Monitoring Date', validators=[DataRequired()])
+    monitoring_date = DateField('Monitoring Date', validators=[DataRequired()], widget=DateInput())
     total_live_oysters = IntegerField('Total Live Oysters', validators=[DataRequired()])
     image = FileField('Measurement Image',validators=[FileRequired()])
     # NOTE: enctype="multipart/form-data" on <form></form> for the .data property to be present on
